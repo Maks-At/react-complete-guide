de7d07e21edc252f750412dd4a576fda3bd6f70b
Maks_At <maks.atagoziyev@gmail.com>
2023-02-10 13:58:01 +0300

    Add chart

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "../ExpenseFilter/ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2019");
@@ -23,6 +24,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onExpenseFilterSelected={selectExpenseFilter}
         />
+        <ExpensesChart expenses={filteredExpenses} />
         <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
